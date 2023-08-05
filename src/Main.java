7bb5ca238f57a48c5347aad39dc7ea1ab7e9d74c
Emmanuel BEMA <bema.emmanuel@yahoo.fr>
2023-08-05 10:34:37 +0100

    Rajout des Etapes

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,22 @@ public class Main {
         System.out.println("Hello world!");
         int livre = 1500;
         String auteur = "BEMA";
+        /**
+         * Etape 1 : Initialisation du repo local
+         *  - git init (pour initialiser)
+         * Etape 2 : Vérification de l'état du repo local
+         *  - git status  (pour verifier)
+         * Etape 3 : Ajout des modifications à l'index
+         *  - git add .  (pour ajouter les modifs)
+         * Etape 4 : Création d'une version du code source
+         *  - git commit -m "description du commit"
+         * Etape 5 : Création du repo en ligne sur Github
+         *
+         *  Etape 6 : Liaison du repo distant au repo local
+         *
+         *  Etape 7 : Synchronisation des 2 environnements
+         */
+
 
     }
 }
